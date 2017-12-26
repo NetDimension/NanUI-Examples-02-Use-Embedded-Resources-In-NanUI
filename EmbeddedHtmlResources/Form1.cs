d5ad13c56d7a9c65181c56b4037840f3ee69999c
林选臣 <xuanchenlin@msn.com>
2017-12-26 23:46:53 +0800

    添加项目文件。

---
+++ EmbeddedHtmlResources/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmbeddedHtmlResources
+{
+	using NetDimension.NanUI;
+
+	public partial class Form1 : Formium
+	{
+		public Form1() 
+			: base("http://my.resource.local/index.html",false)
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ EmbeddedHtmlResources/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmbeddedHtmlResources
+{
+	using NetDimension.NanUI;
+
+	static class Program
+	{
+
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			//指定CEF架构和文件目录结构，并初始化CEF
+			if (Bootstrap.Load(PlatformArch.Auto, System.IO.Path.Combine(Application.StartupPath, "fx"), System.IO.Path.Combine(Application.StartupPath, "fx\\Resources"), System.IO.Path.Combine(Application.StartupPath, "fx\\Resources\\locales")))
+			{
+				//注册嵌入资源，并为指定资源指定一个假的域名my.resource.local
+				Bootstrap.RegisterAssemblyResources(System.Reflection.Assembly.GetExecutingAssembly(), "my.resource.local");
+
+				Application.Run(new Form1());
+			}
+
+		}
+	}
+}
